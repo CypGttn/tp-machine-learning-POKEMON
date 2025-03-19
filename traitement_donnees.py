649a8cd94ca7b21d362339c8b66bd40225e744c6
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-03-19 13:43:11 +0100

    Update traitement_donnees.py

--- traitement_donnees.py
+++ traitement_donnees.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('Pokemon.csv')
+csv_name = 'Pokemon.csv'
+df = pd.read_csv(csv_name)
 
-print(df.to_string()) 
+print(df.head()) 
+
+# Choix des variables inutiles 
+
+useless_var = ['#', 'Name', 'Type 1', 'Type 2', 'Generation'] # Peut être sujet à modification
+df_clean = df.drop(useless_var, axis=1)
+print(df_clean.head())
